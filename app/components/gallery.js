0105350cb67c02ce268428a0b66171c1e1874e11
tenda-ti <160717699+tenda-ti@users.noreply.github.com>
2024-11-19 14:34:52 +0200

    Update project with latest changes

--- app/components/gallery.js
+++ app/components/gallery.js
@@ -3,54 +3,54 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class GalleryComponent extends Component {
-    @tracked selectedImage = null;
-    @tracked isModalOpen = false;
-    @tracked currentPage = 1;
-    itemsPerPage = 8;
+  @tracked selectedImage = null;
+  @tracked isModalOpen = false;
+  @tracked currentPage = 1;
+  itemsPerPage = 8;
 
-    get paginatedImages() {
-        const start = 0;
-        const end = this.currentPage * this.itemsPerPage;
-        return this.args.images.slice(start, end);
-    }
+  get paginatedImages() {
+    const start = 0;
+    const end = this.currentPage * this.itemsPerPage;
+    return this.args.images.slice(start, end);
+  }
 
-    get hasMoreImages() {
-        return this.paginatedImages.length < this.args.images.length;
-    }
+  get hasMoreImages() {
+    return this.paginatedImages.length < this.args.images.length;
+  }
 
-    get canShowLess() {
-        return this.currentPage > 1;
-    }
+  get canShowLess() {
+    return this.currentPage > 1;
+  }
 
-    @action
-    showImage(image) {
-        this.selectedImage = image;
-        this.isModalOpen = true;
-        document.body.style.overflow = 'hidden';
-    }
+  @action
+  showImage(image) {
+    this.selectedImage = image;
+    this.isModalOpen = true;
+    document.body.style.overflow = 'hidden';
+  }
 
-    @action
-    closeModal() {
-        this.isModalOpen = false;
-        this.selectedImage = null;
-        document.body.style.overflow = '';
-    }
+  @action
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedImage = null;
+    document.body.style.overflow = '';
+  }
 
-    @action
-    nextPage() {
-        this.currentPage++;
-        window.scrollTo({ 
-            top: document.body.scrollHeight,
-            behavior: 'smooth'
-        });
-    }
+  @action
+  nextPage() {
+    this.currentPage++;
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth',
+    });
+  }
 
-    @action
-    showLess() {
-        this.currentPage--;
-        window.scrollTo({ 
-            top: 0,
-            behavior: 'smooth'
-        });
-    }
+  @action
+  showLess() {
+    this.currentPage--;
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,17 +2,16 @@ import Route from '@ember/routing/route';
 import fetch from 'fetch';
 
 export default class IndexRoute extends Route {
-    async model() {
-        const response = await fetch('/assets/images/gallery.json');
-        const data = await response.json();
-        return {
-            images: [
-                '/assets/images/img1.jpg',
-                '/assets/images/img2.jpg',
-                '/assets/images/sofa1.jpg',
-             
-                 ],
-            galleryImages: data.gallery 
-        };
-    }
+  async model() {
+    const response = await fetch('/assets/images/gallery.json');
+    const data = await response.json();
+    return {
+      images: [
+        '/assets/images/img1.jpg',
+        '/assets/images/img2.jpg',
+        '/assets/images/sofa1.jpg',
+      ],
+      galleryImages: data.gallery,
+    };
+  }
 }
